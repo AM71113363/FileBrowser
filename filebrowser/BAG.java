1acb71f7f3a78a0033c4dc72de8efb9c4af23d74
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2024-08-28 12:41:59 +0200

    Update BAG.java

--- filebrowser/BAG.java
+++ filebrowser/BAG.java
@@ -2,24 +2,23 @@ package sss.am71113363.filebrowser;
 //Copyright AM71113363
 public class BAG
 {
-	private String name;  //fileName (not a path)
-	private boolean isFile; //false for Dir,true for File
-	private boolean isBlur;
-	//@Override
-	public BAG(String name)
-	{
-		this.name=name;
-		isFile=false;
-		isBlur = false;
-	}
+    private String name;  //fileName (not a path)
+    private boolean isFile; //false for Dir,true for File
+    private boolean isBlur;
+    //@Override
+    public BAG(String name)
+    {
+        this.name=name;
+	isFile=false;
+	isBlur = false;
+    }
     public String getName()
-	{
-		return name;
-	}
-	public boolean isFile() { return isFile; }
-	public boolean isBlur() { return isBlur; }
+    {
+        return name;
+    }
+    public boolean isFile() { return isFile; }
+    public boolean isBlur() { return isBlur; }
 
-	public void isFile(boolean b)  { isFile=b;  }
-	public void isBlur(boolean b)  { isBlur=b;  }
+    public void isFile(boolean b)  { isFile=b;  }
+    public void isBlur(boolean b)  { isBlur=b;  }
 }
-
