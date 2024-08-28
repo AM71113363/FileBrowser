02f6a1119cb4dc2b2fdbf50942f7075ea71c702f
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2024-08-28 11:38:20 +0200

    Add files via upload

---
+++ filebrowser/DATABASE.java
@@ -0,0 +1,93 @@
+package sss.am71113363.filebrowser;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import java.util.*;
+import java.io.*;
+
+public abstract class DATABASE
+{
+	private static String home="storage/emulated/0";
+	public static List<String> history=new ArrayList<String>();
+	
+	public static List<BAG> list= new ArrayList<BAG>();
+
+	//history
+	public static void addIfEmpty(String entry)
+	{
+		if(history.size()==0)
+		{
+		    history.add(entry);
+		}
+	}
+	public static boolean RemoveLastHistory()
+	{
+		int len=history.size();
+		
+		if(len<=1) //only 1,its the root
+			return false;//removing root is not allowed
+		
+		history.remove(len-1);
+	   return true;
+	}
+	public static void add(String entry)
+	{
+		history.add(entry);
+	}
+	public static List<String> GetHistory()
+	{
+		return history;
+	}
+	public static String GetHistory(int index)
+	{
+		return history.get(index);
+	}
+	public static int GetHistoryLen()
+	{
+		return history.size();
+	}
+	public static String GetLastHistory()
+	{
+		int len=history.size();
+		if(len == 0)
+		{
+			history.add(home);
+			return home;
+		}
+		return history.get(len-1);
+	}
+	
+	//list
+	public static void add(BAG entry)
+	{
+		list.add(entry);
+	}
+	public static List<BAG> GetList()
+	{
+		return list;
+	}
+	public static int GetListLen()
+	{
+		return list.size();
+	}
+	public static BAG GetList(int index)
+	{
+		return list.get(index);
+	}
+	
+	//reset
+	public static void ResetList()
+	{
+		list.clear();
+	}
+	public static void ResetHistory()
+	{
+		history.clear();
+	}
+	
+}
---
+++ filebrowser/FCallback.java
@@ -0,0 +1,24 @@
+package sss.am71113363.filebrowser;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import android.util.*;
+import android.text.*;
+import android.graphics.drawable.*;
+import android.graphics.drawable.shapes.*;
+import android.transition.*;
+
+public interface FCallback
+{
+	public boolean isFileBlur(String name);
+	public char getFileSeparator();
+	public void DONE(String path,boolean isCancel);
+}
